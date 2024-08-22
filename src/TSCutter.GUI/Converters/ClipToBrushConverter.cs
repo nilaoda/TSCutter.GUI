48ff1487c76ad3364b1195fa27152934186a1df1
nilaoda <nilaoda@live.com>
2024-08-22 23:56:08 +0800

    fix dark mode color

--- src/TSCutter.GUI/Converters/ClipToBrushConverter.cs
+++ src/TSCutter.GUI/Converters/ClipToBrushConverter.cs
@@ -19,12 +19,16 @@ public class ClipToBrushConverter : IMultiValueConverter
             StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative),
             EndPoint = new RelativePoint(1, 0, RelativeUnit.Relative),
         };
-        
+
         if (values[0] is not double durationMax ||
-            values[1] is not double startTime || 
-            values[2] is not double endTime || 
+            values[1] is not double startTime ||
+            values[2] is not double endTime ||
             durationMax <= 0)
+        {
+            gradient.GradientStops.Add(new GradientStop(Colors.Transparent, 0));
+            gradient.GradientStops.Add(new GradientStop(Colors.Transparent, 1));
             return gradient;
+        }
 
         // scale to 0-1
         var highlightStart = startTime;
