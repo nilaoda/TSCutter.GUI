19d6a5eb6c8c0167d60740f61c8af4c6d9b725a0
nilaoda <nilaoda@live.com>
2025-11-19 23:12:40 +0800

    Fix duration calc (#29)

--- src/TSCutter.GUI/ViewModels/MainWindowViewModel.cs
+++ src/TSCutter.GUI/ViewModels/MainWindowViewModel.cs
@@ -36,7 +36,7 @@ public partial class MainWindowViewModel : ViewModelBase
     {
         get
         {
-            var defaultTitle = "TSCutter.GUI - Alpha.251108";
+            var defaultTitle = "TSCutter.GUI - Alpha.251119";
             if (!string.IsNullOrEmpty(VideoPath))
                 return $"{defaultTitle} - {Path.GetFileName(VideoPath)}";
             return defaultTitle;
