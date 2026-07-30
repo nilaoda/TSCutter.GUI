99910025fa277c155c71eeef6d124b5fb0bfd593
nilaoda <nilaoda@live.com>
2026-07-30 22:22:59 +0800

    chore: bump version for 20260730 alpha build

--- src/TSCutter.GUI/App.axaml.cs
+++ src/TSCutter.GUI/App.axaml.cs
@@ -18,7 +18,7 @@ namespace TSCutter.GUI;
 
 public partial class App : Application
 {
-    public const string CurrentTag = "alphabuild_20260727";
+    public const string CurrentTag = "alphabuild_20260730";
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
